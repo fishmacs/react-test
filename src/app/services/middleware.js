f76dcc127f78efa9e2c6acefbc7a257c9fcda47d
zw <wzhao1984@gmail.com>
2016-06-14 18:10:09 +0800

    redux-logger middleware

--- src/app/services/middleware.js
+++ src/app/services/middleware.js
@@ -1,11 +1,13 @@
 import promiseMiddleware from 'redux-promise-middleware'
 import thunk from 'redux-thunk'
 import createSagaMiddleware from 'redux-saga'
+//import createLogger from 'redux-logger'
 
 export const saga = createSagaMiddleware()
 
 export const middlewares = [
   thunk,
   promiseMiddleware(),
-  saga
+  saga,
+  //createLogger()
 ]
