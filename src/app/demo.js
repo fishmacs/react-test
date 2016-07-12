fdebe6b6a6f5dac81f003c69db0731911ae2569c
zw <wzhao1984@gmail.com>
2016-07-12 17:58:46 +0800

    move Main to from demo.js to entry.js

--- src/app/demo.js
+++ src/app/demo.js
@@ -1,35 +1,5 @@
-import { Router, IndexLink, Link, browserHistory, createMemoryHistory } from 'react-router'
-import { Provider } from 'react-redux'
-import { trigger } from 'redial'
-
-import { isBrowser } from 'app/utils'
-import routes from 'app/routes'
-import store from 'app/services/store'
+import { IndexLink, Link } from 'react-router'
 import styles from 'app/styles/demo.css'
-
-const history = isBrowser ? browserHistory : createMemoryHistory()
-
-function routeLocalsTrigger(event) {
-  return function() {
-    const dispatch = store.dispatch
-    const { components, location, params } = this.state
-    trigger(event, components, { dispatch, location, params })
-  }
-}
-
-const onRouteUpdate = routeLocalsTrigger('defer')
-
-export class Main extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router history={history} onUpdate={onRouteUpdate}>
-          { routes() }
-        </Router>
-      </Provider>
-    )
-  }
-}
 
 export class Demo extends React.Component {
   render() {
@@ -54,3 +24,4 @@ export class Demo extends React.Component {
     )
   }
 }
+
--- src/app/entry.js
+++ src/app/entry.js
@@ -1,6 +1,35 @@
 import ReactDOM from 'react-dom'
+import {Router, browserHistory, createMemoryHistory} from 'react-router'
+import { Provider } from 'react-redux'
+import { trigger } from 'redial'
 
-import { Main } from 'app/demo'
+import { isBrowser } from 'app/utils'
+import store from 'app/services/store'
+import routes from 'app/routes'
+
+const history = isBrowser ? browserHistory : createMemoryHistory()
+
+function routeLocalsTrigger(event) {
+  return function() {
+    const dispatch = store.dispatch
+    const { components, location, params } = this.state
+    trigger(event, components, { dispatch, location, params })
+  }
+}
+
+const onRouteUpdate = routeLocalsTrigger('defer')
+
+export class Main extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router history={history} onUpdate={onRouteUpdate}>
+          { routes }
+        </Router>
+      </Provider>
+    )
+  }
+}
 
 ReactDOM.render(<Main/>, document.getElementById('application-root'))
 
